dce5b928a42942c7c14d253dbeb340cb66870641
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2025-10-07 17:17:22 +0300

    Better ex09

--- Lab01/e09.py
+++ Lab01/e09.py
@@ -1,10 +1,15 @@
 stringCamelCase = "UpperCamelCaseString"
 stringWithUnderscores = ""
+index = 0
 
 for char in stringCamelCase:
-    if char.isupper():
+    if char.isupper() and index == 0:
+        stringWithUnderscores += char.lower()
+    elif char.isupper():
         stringWithUnderscores +=  "_" + char.lower()
     else:
         stringWithUnderscores += char
+    index += 1
 
+print(stringCamelCase)
 print(stringWithUnderscores)
