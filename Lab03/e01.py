faff19095574a63b6b65711c962cfde256a6792f
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2025-10-21 17:15:10 +0300

    Small change

--- Lab03/e01.py
+++ Lab03/e01.py
@@ -1,10 +1,10 @@
 myString = "Hello World"
 
-def StringToDict(string):
+def string_to_dict(string):
     letter_dict = {}
     for char in string:
         letter_dict[char] = letter_dict.get(char, 0) + 1
 
     return letter_dict
 
-print(StringToDict(myString))
+print(string_to_dict(myString))
